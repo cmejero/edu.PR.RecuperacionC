fcda9519c1bbbe9cd3cdfc6e81b0f1cca8eedebe
DESKTOP-5PHL67U\Carlos <csi23-cmejero@colegioaltair.net>
2024-05-29 10:06:44 +0200

    Agregar archivos de proyecto.

---
+++ Servicios/FicherosImplementacion.cs
@@ -0,0 +1,82 @@
+﻿using EduRecuperacionC.Controladores;
+using EduRecuperacionC.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduRecuperacionC.Servicios
+{
+    /// <summary>
+    /// clase de que se encarga de implementar fichero interfaz
+    /// </summary>
+    internal class FicherosImplementacion : FicherosInterfaz
+    {
+        public void escribirFichero(string mensaje)
+        {
+            // poner en null porque debe tener un valor, se declara fuera del try ya que dentro no lo cogeria el finally.
+            StreamWriter st = null;
+
+            try
+            {
+                st = new StreamWriter(Program.rutaFicheroLog, true);
+                st.WriteLine(mensaje);
+
+
+            }
+            catch (IOException io)
+            {
+                Console.WriteLine("Ha ocurrido un error en ficheros, intentelo más tarde. error(1001)" + io.Message);
+            }
+            finally
+            {
+                if (st != null)
+                {
+                    st.Close();
+                }
+            }
+        } 
+        
+
+
+        public void escribirAlumnos()
+        {
+            
+            StreamWriter st = null;
+
+            try
+            {
+
+                st= new StreamWriter(Program.rutaFicheroAlumno, true);
+                foreach (AlumnoDto alumno in Program.listaAlumnos)
+                {
+
+                    st.WriteLine(alumno.ToString(';'));
+                    
+
+                }
+            }catch (IOException io)
+            {
+
+                // llamar al metodo escribir fichero para escribir en ficheroLog si hay algun error
+                escribirFichero("ha ocurrido un error, intentelo más tarde" + io.Message);
+                
+                Console.WriteLine("Ha ocurrido un error en ficheros, intentelo más tarde. error(1001)" + io.Message);
+            }finally
+            {
+                if (st != null)
+                {
+                    st.Close();
+                }
+            }
+            
+
+        }
+        
+
+        
+
+    }
+}
---
+++ Servicios/FicherosInterfaz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduRecuperacionC.Servicios
+{
+
+    /// <summary>
+    /// clase que se encarga de los metodos relaciondos con ficheros
+    /// </summary>
+    internal interface FicherosInterfaz
+    {
+        /// <summary>
+        /// metodo que se encarga de escribir en un fichero.
+        /// </summary>
+        /// <param name="mensaje"></param>
+        public void escribirFichero(string mensaje);
+
+
+        /// <summary>
+        /// metodo que se encarga en escribir en fichero los alumnos de la lista
+        /// </summary>
+        public void escribirAlumnos();
+    }
+}
